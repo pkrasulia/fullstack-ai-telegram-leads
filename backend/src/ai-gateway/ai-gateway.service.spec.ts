5cf0634c8f2b5b1ad10e8e577e4eec055aed3222
pkrasulia <pkrasulia@gmail.com>
2025-09-22 17:55:29 +0300

    test: add basic test for module ai-gateway

--- backend/src/ai-gateway/ai-gateway.service.spec.ts
+++ backend/src/ai-gateway/ai-gateway.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AiGatewayService } from './ai-gateway.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('AiGatewayService', () => {
   let service: AiGatewayService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AiGatewayService],
+      providers: [
+        AiGatewayService,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn().mockReturnValue(undefined),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AiGatewayService>(AiGatewayService);
